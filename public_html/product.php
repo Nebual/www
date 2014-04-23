c0144c33b19493df586df43b78a98c75f518c051
sinter <mtb_kj@hotmail.com>
2014-04-22 20:50:03 -0700

    Replaced single quotes with HTML escaped versions

--- public_html/product.php
+++ public_html/product.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Wally's Widget World - Product A</title>
+<title>Wally&#39;s Widget World - Product A</title>
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="common.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -17,13 +17,13 @@
 </style>
 </head>
 <body>
-<h1>Wally's Widget World</h1>
+<h1>Wally&#39;s Widget World</h1>
 <ol class="breadcrumb">
   <li><a href="index.php">Home</a></li>
   <li><a href="category.php?id=1">Category A</a></li>
   <li class="active"><a href="product.php?id=1">Product A</a></li>
 </ol>
-What I gotta tell you about a product too? Okay well heres what we've parsed so far:
+What I gotta tell you about a product too? Okay well heres what we&#39;ve parsed so far:
 <table class="table table-bordered" id="productinfo">
 	<tr><th>Model</th><td>T989</td></tr>
 	<tr><th>Price</th><td>a lot</td></tr>
